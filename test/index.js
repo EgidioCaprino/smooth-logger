1676e1f7b3ff10dbe9a559ea6c912026c636f350
Egidio Caprino <egidio.caprino@gmail.com>
2018-11-30 22:38:27 +0100

    test: createLogger and .log method

--- test/index.js
+++ test/index.js
@@ -8,3 +8,23 @@ describe('createLogger', () => {
     }));
   });
 });
+
+describe('.log', () => {
+  it('should log into console when enabled', () => {
+    global.console = {
+      log: jest.fn(),
+    };
+    const messages = ['first', 'second', 'third'];
+    createLogger(true).log(...messages);
+    expect(global.console.log).toHaveBeenCalledWith(...messages);
+  });
+
+  it('should not log into console when disabled', () => {
+    global.console = {
+      log: jest.fn(),
+    };
+    const messages = ['first', 'second', 'third'];
+    createLogger(false).log(...messages);
+    expect(global.console.log).toHaveBeenCalledTimes(0);
+  });
+});
